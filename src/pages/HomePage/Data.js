502338f63d94a80b704aee837d9609511638adb5
kseto2 <kseto2@edu.uwaterloo.ca>
2020-09-12 22:00:14 -0400

    Home page Addition

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,13 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    lightBg: false,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Check Projects',
+    description: 'Thank you for checking out my website.',
+    headline: 'Hello there',
+    lightText: true,
+    topLine: 'Kyle Seto' 
+};
