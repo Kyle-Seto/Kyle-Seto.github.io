2f4f82507e8febb517b6fe8ed30a8915381c4eb2
kseto2 <kseto2@edu.uwaterloo.ca>
2020-09-12 20:14:10 -0400

    Navbar Btn addition

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib'
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from './Navbar.elements';
+import { Button } from '../../globalStyles'
+import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon, NavMenu, NavItem, NavLinks, NavItemBtn, NavBtnLink } from './Navbar.elements';
 
 const Navbar = () => {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click)
-     
+    
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton()
+    }, []);
+
+    window.addEventListener('resize',showButton);
     return (
         <>
         <IconContext.Provider value={{ color: '#fff' }}>
@@ -15,11 +30,37 @@ const Navbar = () => {
                 <NavbarContainer>
                     <NavLogo to="/">
                         <NavIcon />
-                        ULTRA
+                        KYLE SETO
                     </NavLogo>
                     <MobileIcon onClick={handleClick}>
                         {click ? <FaTimes /> : <FaBars />}
                     </MobileIcon>
+                    <NavMenu onClick = {handleClick} click={click}>
+                        <NavItem>
+                            <NavLinks to='/'>Home</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='/Projects'>Projects</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='/About'>About</NavLinks>
+                        </NavItem>
+                        <NavItemBtn>
+                            {button ? (
+                                <NavBtnLink to="/Contact Me">
+                                    <Button primary>Contact Me</Button>
+                                </NavBtnLink>
+                            ) : (
+                                <NavBtnLink to="/Contact Me">
+                                    <Button fontBig primary>
+                                        Contact Me
+                                    </Button>
+                                </NavBtnLink>
+                            )} 
+                        </NavItemBtn>
+                    </NavMenu>
+                   
+                    
                 </NavbarContainer>
             </Nav>
         </IconContext.Provider>
