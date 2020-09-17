0be2e3d316118aa1f5d85bbd66d5a86260ac683a
kseto2 <kseto2@edu.uwaterloo.ca>
2020-09-16 21:25:12 -0400

    Footer addition

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
 import { FooterContainer, FooterSubscription, FooterSubheading, FooterSubText, Form, FormInput, 
-    FooterLinksContainer, FooterLinksWrapper, FooterLinksItems, FooterLinkTitle, FooterLink } from './Footer.elements';
+    FooterLinksContainer, FooterLinksWrapper, FooterLinksItems, FooterLinkTitle, FooterLink, SocialMedia,
+SocialMediaWrap,SocialLogo, WebsiteRights, SocialIcons, SocialIconLink, SocialIcon } from './Footer.elements';
 import { Button } from '../../globalStyles';
+
 const Footer = () => {
     return (
         <FooterContainer>
@@ -35,6 +38,8 @@ const Footer = () => {
                         <FooterLink to='/Contact-me'>Hi</FooterLink>
                         <FooterLink to='/Contact-me'>Hi</FooterLink>
                     </FooterLinksItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
                     <FooterLinksItems>
                         <FooterLinkTitle>About Us </FooterLinkTitle>
                         <FooterLink to='/Contact-me'>Hi</FooterLink>
@@ -59,15 +64,15 @@ const Footer = () => {
                         <SocialIcon/> 
                         ULTRA
                         </SocialLogo>
-                        <WebsiteRights>ULTRA  copyrightsign 2020</WebsiteRights>
+                        <WebsiteRights>ULTRA © 2020</WebsiteRights>
                     <SocialIcons>
-                        <SocialIconLink href='/' target="_blank" aria-label="Github">
+                        <SocialIconLink href={'//github.com/kyleseto2'} target="_blank" aria-label="Github" rel="noopener noreferrer">
                             <FaGithub />
                         </SocialIconLink> 
-                        <SocialIconLink href='/' target="_blank" aria-label="LinkedIn">
-                            <FaLinkedIn />
+                        <SocialIconLink href={'//www.linkedin.com/in/kyle-seto2'} target="_blank" aria-label="Linkedin" rel="noopener noreferrer">
+                            <FaLinkedin />
                         </SocialIconLink> 
-                        <SocialIconLink href='/' target="_blank" aria-label="Instagram">
+                        <SocialIconLink href={'//www.instagram.com/kyle.seto'} target="_blank" aria-label="Instagram" rel="noopener noreferrer">
                             <FaInstagram />
                         </SocialIconLink> 
                     </SocialIcons>
