00664f41273bdcdb80be105d46cae2c48060e123
kseto2 <kseto2@edu.uwaterloo.ca>
2020-09-17 19:32:24 -0400

    Updates

---
+++ precache-manifest.1febe1a8a1a7e2274739271f283f9d2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dea6d4b011b0ece7dee35e3e5db727c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6636975e38661bbbf687",
+    "url": "/static/js/2.6d881f52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d881f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725eaf0c746d000c2055",
+    "url": "/static/js/main.1bc088f6.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "40bf36f12c25fd021a693bfaf7185072",
+    "url": "/static/media/about.40bf36f1.svg"
+  },
+  {
+    "revision": "64fb8a6c031e021a72de8ea970588908",
+    "url": "/static/media/contact.64fb8a6c.svg"
+  },
+  {
+    "revision": "d0b02b18ee2bec544a90edadcca5a8eb",
+    "url": "/static/media/home.d0b02b18.svg"
+  }
+]);
